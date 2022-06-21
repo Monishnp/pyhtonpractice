6a444281ba0c01958c4d6be3409b8b88e24703c1
Monishnp <107698875+Monishnp@users.noreply.github.com>
2022-06-21 12:59:04 +0530

    file added

---
+++ my _practice.py
@@ -0,0 +1,18 @@
+a=5
+b=6
+c=(a+b)
+print('Sum of variable is ' + str(int(c)));
+
+print ("what is your name ?")
+myname=input()
+print ("My name is " + myname)
+print("Nice to meet you " + myname)
+size= len(myname)
+print("length of your name is " + str(int(size)) )
+
+print ("what is you age ?")
+myage=input()
+print(myname + " you will be " + str(int(myage) + 1) +  " after a year ")
+print('what is you height ?')
+height=input()
+print (" your height is "+ str(float(height)))
